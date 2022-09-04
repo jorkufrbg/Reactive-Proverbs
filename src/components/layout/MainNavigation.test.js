0460040c69de3b8119b7143efd7bee4e596f6ed9
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-09-04 21:41:27 +0300

    Unit Tests

---
+++ src/components/layout/MainNavigation.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import MainNavigation from "./MainNavigation";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Main Navigation Component", () => {
+  test("Renders Header Element", () => {
+    render(<MainNavigation />, { wrapper: BrowserRouter });
+    expect(screen.getByText("Great Quotes")).toBeInTheDocument();
+  });
+
+  test("Renders Navigation Menu", () => {
+    render(<MainNavigation />, { wrapper: BrowserRouter });
+    expect(screen.getByRole("navigation")).toBeInTheDocument();
+  });
+});

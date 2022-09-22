f2400f4d301d70d3122796566ebcc1818a3a2c8e
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-09-22 17:32:35 +0300

    Fixed jest test file extensions ( *.test.js > *.test.tsx)

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <BrowserRouter>
     <App />

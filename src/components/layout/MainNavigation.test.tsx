338d82f05e68c23db65f05d0e026159e1596763c
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-10-13 20:18:35 +0300

    Unit Test Adjustments

--- src/components/layout/MainNavigation.test.tsx
+++ src/components/layout/MainNavigation.test.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 describe('Main Navigation Component', () => {
   test('Renders Header Element', () => {
     render(<MainNavigation />, { wrapper: BrowserRouter })
-    expect(screen.getByText('Great Quotes')).toBeInTheDocument()
+    expect(screen.getByText('Reactive Quotes')).toBeInTheDocument()
   })
 
   test('Renders Navigation Menu', () => {

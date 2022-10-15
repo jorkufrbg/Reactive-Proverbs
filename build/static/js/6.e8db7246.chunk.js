9209bc10af13bcca959918275e2d6f88b605c550
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-10-15 14:50:13 +0300

    Delete build directory

--- build/static/js/6.e8db7246.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-quotes"]=this["webpackJsonpreact-quotes"]||[]).push([[6],{54:function(e,t,c){"use strict";c.r(t);var s=c(1);t.default=function(){return Object(s.jsx)("div",{className:"centered",children:Object(s.jsx)("p",{children:"Page Not Found!"})})}}}]);
-//# sourceMappingURL=6.e8db7246.chunk.js.map
